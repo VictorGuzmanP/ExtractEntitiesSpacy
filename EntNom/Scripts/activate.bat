16c78f31d780da0969ad56dde89fadd083687a93
VictorGuzmanP <victorguzman_1982@yahoo.es>
2022-04-10 13:53:00 +0100

    main

--- EntNom/Scripts/activate.bat
+++ EntNom/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\IES Severo Ochoa\IABD\EntidadesNombradas\EntNom
+set VIRTUAL_ENV=D:\IES Severo Ochoa\IABD\ExtractEntitiesSpacy\EntNom
 
 if not defined PROMPT set PROMPT=$P$G
 
